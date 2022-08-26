a4872576c7dcd64076ddfbdc1111d08bac877af3
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2022-08-26 23:13:21 +0530

    2nd commit

--- app/src/main/java/com/example/swiperecyclerview/MainActivity.java
+++ app/src/main/java/com/example/swiperecyclerview/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.swiperecyclerview;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
@@ -21,20 +28,35 @@ import java.util.List;
 import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class MainActivity extends AppCompatActivity {
-RecyclerView recyclerView;
+    ImageButton imageButton;
+    Toolbar toolbar; TextView textView;
+    public int position=-1;
+    public boolean isActionmode=false;
+    RecyclerView recyclerView;
 ArrayList<String > arrayList=new ArrayList<>();
+    ArrayList<String> userSelection=new ArrayList<>();
+    int counter=0;
 MainAdapter adapter; String deleted=null;
 List<String> archived=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.tool);setSupportActionBar(toolbar);
+        textView=findViewById(R.id.tt);textView.setVisibility(View.GONE);
+        imageButton=findViewById(R.id.bt); imageButton.setVisibility(View.GONE);
         recyclerView=(RecyclerView)findViewById(R.id.rec);
         arrayList.addAll(Arrays.asList("Java1","Java2","Java3","Java4","Java5","Java6","Java7","Java8","Java9","Java10","Java11","Java12","Java13","Java14","Java15",
                 "Java16","Java17","Java18","Java19","Java20"));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter=new MainAdapter(this,arrayList);
         recyclerView.setAdapter(adapter);
+         imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearActionMode();
+            }
+        });
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT|ItemTouchHelper.LEFT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
@@ -84,4 +106,84 @@ List<String> archived=new ArrayList<>();
 
          }
             }
-        }).attachToRecyclerView(recyclerView); }}
+        }).attachToRecyclerView(recyclerView); }
+
+    private void clearActionMode() {
+        isActionmode=false;
+        textView.setVisibility(View.GONE);
+        textView.setText("0 item selected");
+        counter=0;userSelection.clear();
+        toolbar.getMenu().clear();
+        adapter.notifyDataSetChanged();
+    }
+
+
+    public  void check(View view ,int index){
+        try{
+            if (((CheckBox) view).isChecked()) {
+                userSelection.add(arrayList.get(index));
+                counter++;
+                updateToolBarText(counter);
+            } else {
+                userSelection.remove(arrayList.get(index));
+                counter--;
+                updateToolBarText(counter);
+            }
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+    public  void  startSelection(int index){
+        try  {
+            if (!isActionmode) {
+                isActionmode = true;
+                userSelection.add(arrayList.get(index));
+                counter++;
+                updateToolBarText(counter);
+                textView.setVisibility(View.VISIBLE);
+                imageButton.setVisibility(View.VISIBLE);
+                toolbar.inflateMenu(R.menu.rec_delete);
+                position = index;
+                adapter.notifyDataSetChanged();
+            }
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+    private  void updateToolBarText(int counter) {
+        if (counter==0)
+            textView.setText("0 items selected");
+        else if (counter==1)
+            textView.setText("1 items selected");
+        else
+            textView.setText(counter+" Items selected");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.delete && userSelection.size()>0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Delete "+userSelection.size()+" items ?");
+            builder.setTitle("Confirm");
+            builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            builder.show();
+            for (String s : userSelection){
+                arrayList.remove(s);
+            }
+            updateToolBarText(0);
+            clearActionMode();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/swiperecyclerview/MainAdapter.java
+++ app/src/main/java/com/example/swiperecyclerview/MainAdapter.java
@@ -1,40 +1,114 @@
 package com.example.swiperecyclerview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
-    Activity activity;
+    Context context;
     ArrayList<String> arrayList;
+     MainActivity mainActivity;
 
-    public MainAdapter(Activity activity, ArrayList<String> arrayList) {
-        this.activity = activity;
-        this.arrayList = arrayList; }@NonNull @Override
+    public MainAdapter(Context context, ArrayList<String> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+        this.mainActivity=(MainActivity)context;
+    }
+
+    @NonNull @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_selectable_list_item,parent,false);
-        return new ViewHolder(view); }@Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_items,parent,false);
+        return new ViewHolder(view,mainActivity); }
+        @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
 holder.textView.setText(arrayList.get(position));
+            if (mainActivity.position == position){
+                holder.checkBox.setChecked(true);
+         //  mainActivity.position=-1;
+            }else
+            {
+                holder.checkBox.setChecked(false);
+            }
+//            if (mainActivity.isActionmode){
+//                Anim anim =new Anim(10,holder.linearLayout);
+//                anim.setDuration(300000000);
+//                holder.linearLayout.setAnimation(anim);
+//            }else {
+//                Anim anim =new Anim(0,holder.linearLayout);
+//                anim.setDuration(300);
+//                holder.linearLayout.setAnimation(anim);
+//                holder.checkBox.setChecked(false);
+//            }
+            holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                   mainActivity.startSelection(position);
+                    return true;
+                }
+            });
+            holder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                  mainActivity.check(view,position);
+
+                }
+            });
 holder.itemView.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        Toast.makeText(activity,arrayList.get(holder.getAdapterPosition()),Toast.LENGTH_SHORT).show(); }}); }@Override
+        Toast.makeText(context,arrayList.get(holder.getAdapterPosition()),Toast.LENGTH_SHORT).show(); }}); }@Override
     public int getItemCount() {
         return arrayList.size(); }
         public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
-        public ViewHolder(@NonNull View itemView) {
+            private final MainActivity mainActivity;
+            TextView textView;
+            LinearLayout linearLayout;
+            CardView cardView;
+            CheckBox checkBox;
+        public ViewHolder(@NonNull View itemView,MainActivity mainActivity) {
             super(itemView);
-            textView=itemView.findViewById(android.R.id.text1);
+            checkBox=itemView.findViewById(R.id.chh);
+            linearLayout=itemView.findViewById(R.id.l);
+            cardView=itemView.findViewById(R.id.cdd);
+            textView=itemView.findViewById(R.id.ttt);
+            this.mainActivity=mainActivity;
         }
     }
+//    class Anim extends Animation {
+//        private  int width,startWidth;
+//        private View view;
+//
+//        public Anim(int width, View view) {
+//            this.width = width;
+//            this.startWidth = view.getWidth();
+//            this.view = view;
+//        }
+//
+//        @Override
+//        protected void applyTransformation(float interpolatedTime, Transformation t) {
+//            int newWidth=startWidth+(int)((width-startWidth)*interpolatedTime);
+//            view.getLayoutParams().width=newWidth;
+//            view.requestLayout();
+//            super.applyTransformation(interpolatedTime, t);
+//        }
+//
+//        @Override
+//        public boolean willChangeBounds() {
+//            return true;
+//        }
+//    }
 }
